0ad57cfe7d78e97d000fb5ed4b9d16a97314fd5c
slavov-v <v.slavov96@gmail.com>
2016-11-07 19:00:34 +0200

    Editing base class

--- base_class.py
+++ base_class.py
@@ -1,27 +1,54 @@
 class BaseClass:
-    def __init__(self):
-        pass
+    def __init__(self, health, mana):
+        self.health = health
+        self.mana = mana
+        self.max_health = health
+        self.equipped_weapon = None
+        self.equipped_spell = None
 
-    def is_alive():
-        pass
+    def get_health(self):
+        return self.health
 
-    def can_cast():
-        pass
+    def get_mana(self):
+        return self.mana
 
-    def get_health():
-        pass
+    def is_alive(self):
+        if self.health < 1:
+            return False
+        return True
 
-    def get_mana():
-        pass
+    def can_cast(self, distance_to_enemy):
+        if self.mana >= self.equipped_spell.mana_cost and self.equipped_spell.cast_range >= distance_to_enemy:
+            return True
+        else:
+            return False
 
-    def take_healing():
-        pass
+    def take_damage(self, damage_points):
+        if self.health < damage_points:
+            self.health = 0
+        else:
+            self.health -= damage_points
 
-    def take_mana():
-        pass
+    def take_healing(self, healing_points):
+        if self.is_alive() is False:
+            return False
+        else:
+            if self.health + healing_points > self.max_health:
+                self.health = self.max_health
+            else:
+                self.health += healing_points
+        return True
 
-    def attack():
-        pass
+    def equip(self, weapon):
+        self.equipped_weapon = weapon
 
-    def take_damage():
-        pass
+    def learn(self, spell):
+        self.equipped_spell = spell
+
+    def attack(self, by=None, enemy):
+        if by == 'weapon':
+            if self.equipped_weapon:
+                enemy.health -= self.equipped_weapon
+        if by == 'magic':
+            if self.can_cast():
+                enemy.health -= self.equipped_spell.damage

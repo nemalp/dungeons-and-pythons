796777b3e8ad3b2fe0f9f5d49c4cca9bbd427160
nemalp <lazarov.pl@gmail.com>
2016-11-07 19:40:39 +0200

    Add dugneons

---
+++ dungeons.py
@@ -0,0 +1,11 @@
+class Dungeons:
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def print_map():
+        pass
+
+    def read_file(self):
+        with open(self.filename) as f:
+            data = f.read()

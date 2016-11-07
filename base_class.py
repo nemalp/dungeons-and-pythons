7df62d2e829c95f98a65b71fb3a3d6e15cdd04cd
slavov-v <v.slavov96@gmail.com>
2016-11-07 18:07:05 +0200

    New file maybe

---
+++ base_class.py
@@ -0,0 +1,30 @@
+class BaseClass:
+    def __init__(self):
+        pass
+
+    def is_alive():
+        pass
+
+    def can_cast():
+        pass
+
+    def get_health():
+        pass
+
+    def get_mana():
+        pass
+
+    def take_healing():
+        pass
+
+    def take_mana():
+        pass
+
+    def attack():
+        pass
+
+    def take_damage():
+        pass
+
+    def test():
+        pass

b1a747bdac99ef024991cad29afe17349e0ef972
nemalp <lazarov.pl@gmail.com>
2016-11-12 11:29:26 +0200

    Add helper methods

---
+++ helpers.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def load_map(filename):
+    with open(filename, 'r') as f:
+        data = f.read().split('\n')
+        map_ = [list(x) for x in data if x.strip() != '']
+
+    return map_

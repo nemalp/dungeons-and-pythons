7dc404b7b160780a8dabcc94e25d01df8fca3d9b
slavov-v <v.slavov96@gmail.com>
2016-11-12 14:34:58 +0200

    Fixing writers

--- spell_and_weapon_writers.py
+++ spell_and_weapon_writers.py
@@ -12,17 +12,23 @@ def serialize_and_store_list(list_of_objects, filename):
 
 def main():
     item_type = input("spells or weapons? ")
+    choice = 'y'
     if item_type == 'spells':
-        while((choice=input("Continue?(y/n)")) != 'n'):
+        while choice != 'n':
             name = input("Enter spell name: ")
             damage = int(input("Enter spell damage: "))
             mana_cost = int(input("Enter mana cost: "))
             cast_range = int(input("Enter cast range: "))
             SPELLS.append(Spell(name, damage, mana_cost, cast_range))
+            choice = input("Continue?(y/n)")
         serialize_and_store_list(SPELLS, "spells.src")
     elif item_type == 'weapons':
-        while((choice=input("Continue?(y/n)")) != 'n'):
+        while choice != 'n':
             name = input("Enter weapon name: ")
             damage = int(input("Enter weapon damage: "))
             WEAPONS.append(Weapon(name, damage))
+            choice = input("Continue?(y/n)")
         serialize_and_store_list(WEAPONS, "weapons.src")
+
+if __name__ == '__main__':
+    main()

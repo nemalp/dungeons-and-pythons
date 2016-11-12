07a64fd2e237e49fd802da58edb1740d366281b9
nemalp <lazarov.pl@gmail.com>
2016-11-12 11:31:12 +0200

    Add spell and weapon classes

---
+++ spell_and_weapon.py
@@ -0,0 +1,13 @@
+class Weapon:
+    def __init__(self, name, damage=0):
+        self.name = name
+        self.damage = damage
+
+
+class Spell:
+
+    def __init__(self, name, damage, mana_cost, cast_range):
+        self.name = name
+        self.damage = damage
+        self.mana_cost = mana_cost
+        self.cast_range = cast_range
